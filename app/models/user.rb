634b65f64d192159f9e48fafb11525c744030986
saasbook <veontomo@gmail.com>
2013-03-06 14:50:45 -0800

    all files are added to commit

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :role_ids
+
+
+ def role?(role)
+      return !!self.roles.find_by_name(role.to_s.camelize)
+  end
+
+  # Default role is "registered"
+  def setup_role 
+    if self.role_ids.empty?     
+      self.role_ids = Role.find_by_name("registered").id
+    end
+  end
+end
