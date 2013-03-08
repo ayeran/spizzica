35602fe00389dcc0d5e223e8694a36d6eddb54a1
veontomo <veontomo@t901.(none)>
2013-03-08 23:09:46 +0100

    working version with autorizations

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,22 +1,15 @@
 class Admin::AdminController < ApplicationController
   layout "admin"
-
   before_filter :verify_admin
-  before_filter :authenticate_user!
 
-  def current_ability
-    @current_ability ||= AdminAbility.new(current_user)
-  end
-  
- 
   def verify_admin
     :authenticate_user!
-    redirect_to root_url unless has_role?(current_user, 'admin')
+    # redirect_to root_url unless has_role?(current_user, "admin")
+    redirect_to root_url unless current_user.roles.collect{|r| r.name}.include?("admin")
   end
 
   def current_ability
     @current_ability ||= AdminAbility.new(current_user)
   end
-
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_and_belongs_to_many :roles
+  before_save :setup_role
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -8,17 +11,14 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :role_ids
   
-  has_and_belongs_to_many :roles
-
- before_save :setup_role
- def role?(role)
+  def role?(role)
       return !!self.roles.find_by_name(role.to_s.camelize)
   end
 
   # Default role is "registered"
   def setup_role 
     if self.role_ids.empty?     
-      self.role_ids = Role.find_by_name("registered").id
+      self.role_ids = [3] #Role.find_by_name("registered").id
     end
   end
 end
