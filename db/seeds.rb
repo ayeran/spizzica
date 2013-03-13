781b25c6ce0edc2f447fd92451e4acffbbe6442b
veontomo <veontomo@t901.(none)>
2013-03-13 09:01:24 +0100

    dropdown list and check box are added to beers/_form

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@
 Role.create([{id: 1, name: "root"},{id: 2, name: "admin"},{id: 3, name: "registered"},
   {id: 4,name: "guest"}])
   
+User.create([{[id: 1, email: "veontomo@gmail.com", encrypted_password: "$2a$10$DH.t4UDiQ/ciOjsWuiADLuSKx3ngAIIb/.zy3vh.b92t...", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2013-03-12 22:16:50", last_sign_in_at: "2013-03-12 22:16:50", current_sign_in_ip: "78.13.232.170", last_sign_in_ip: "78.13.232.170", created_at: "2013-03-12 22:16:50", updated_at: "2013-03-12 22:16:50"]}])
+
