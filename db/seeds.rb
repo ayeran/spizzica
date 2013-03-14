9e26854c2486e3ffb2bf4d75b73d4ba6234fa86b
veontomo <veontomo@t901.(none)>
2013-03-14 09:29:20 +0100

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,27 @@
 
 
 Role.create([{id: 1, name: "root"},{id: 2, name: "admin"},{id: 3, name: "registered"},
-  {id: 4,name: "guest"}])
+  {id: 4,name: "guest"}]) unless Role.count > 0
   
-User.create([{[id: 1, email: "veontomo@gmail.com", encrypted_password: "$2a$10$DH.t4UDiQ/ciOjsWuiADLuSKx3ngAIIb/.zy3vh.b92t...", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2013-03-12 22:16:50", last_sign_in_at: "2013-03-12 22:16:50", current_sign_in_ip: "78.13.232.170", last_sign_in_ip: "78.13.232.170", created_at: "2013-03-12 22:16:50", updated_at: "2013-03-12 22:16:50"]}])
+User.create([{id: 1, 
+   email: "veontomo@gmail.com", 
+   encrypted_password: "$2a$10$DH.t4UDiQ/ciOjsWuiADLuSKx3ngAIIb/.zy3vh.b92t...", 
+   reset_password_token: nil, 
+   reset_password_sent_at: nil, 
+   remember_created_at: nil, 
+   sign_in_count: 1, 
+   current_sign_in_at: "2013-03-12 22:16:50", 
+   last_sign_in_at: "2013-03-12 22:16:50", 
+   current_sign_in_ip: "78.13.232.170", 
+   last_sign_in_ip: "78.13.232.170", 
+   created_at: "2013-03-12 22:16:50", 
+   updated_at: "2013-03-12 22:16:50"}]) unless User.find_by_email("veontomo@gmail.com")
 
+
+["tappo a vite", "tappo meccanico"].each do |l|
+  Lid.find_or_create_by_name(l)
+end
+
+["bottiglia","lattina","bottiglia ceramica"].each do |b|
+  Container.find_or_create_by_name(b)
+end
