ab2604c7e27403be1b863dd89cb1bbccf49a066e
Mario <veontomo@gmail.com>
2013-04-05 23:26:32 +0200

    images for ingredients are added

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,10 @@ class Ingredient < ActiveRecord::Base
      has_and_belongs_to_many :items
      has_and_belongs_to_many :sandwiches
      validates :name, :uniqueness => {:case_sensitive => false}
+     has_attached_file :ingredientimage, :styles => {:thumb => "50x50>",
+                                                     :small => "150x150>",
+                                                     :medium => "250x250>"}#,
+                                          # :url =>"assets/ingredients/:id/:style/:basename.:extension",
+                                          # :path =>":rails_root/public/assets/ingredients/:id/:style/:basename.:extension"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130403201929) do
+ActiveRecord::Schema.define(:version => 20130405175604) do
 
   create_table "aromas", :force => true do |t|
     t.string   "name"
@@ -91,6 +91,10 @@ ActiveRecord::Schema.define(:version => 20130403201929) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ingredientimage_file_name"
+    t.string   "ingredientimage_content_type"
+    t.integer  "ingredientimage_file_size"
+    t.datetime "ingredientimage_updated_at"
   end
 
   create_table "ingredients_sandwiches", :id => false, :force => true do |t|
