7186b866f541fbdfd921129a440b8179324d7490
Mario <veontomo@gmail.com>
2013-04-16 13:39:01 +0200

    migration 20130410104810 modified: :ingredients is replaced by :ingredient

--- db/migrate/20130410104810_remove_reference_from_item.rb
+++ db/migrate/20130410104810_remove_reference_from_item.rb
@@ -1,7 +1,7 @@
 class RemoveReferenceFromItem < ActiveRecord::Migration
   def change
     change_table :items do |t|
-      t.remove_references :ingredients
+      t.remove_references :ingredient
     end
   end
 end
