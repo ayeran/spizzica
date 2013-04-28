d93e17cd3c564f9941abe846fd8a66395239b784
Mario <veontomo@gmail.com>
2013-04-28 19:35:02 +0200

    No manufacturer is displayed in beers/index.html

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
-
-  def ordersaved(order)
-    mail(:to => "veontomo@gmail.com", :subject => "la conferma da Spizzicaluna!")
-  end
+  # default from: "from@example.com"
+#
+  # def ordersaved(order)
+    # mail(:to => "veontomo@gmail.com", :subject => "la conferma da Spizzicaluna!")
+  # end
 end
