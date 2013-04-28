03304392676661e4d0717d52286e912a01862c54
Mario <veontomo@gmail.com>
2013-04-28 15:51:41 +0200

    mailer is updated

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def welcome_email
+  def ordersaved(order)
     mail(:to => "veontomo@gmail.com", :subject => "la conferma da Spizzicaluna!")
   end
 end
