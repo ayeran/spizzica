0282f53cdc2fbe202616bcd3e3d2465367e0b136
Mario <veontomo@gmail.com>
2013-05-07 17:31:16 +0200

    shopping cart is added to layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
+#!/bin/env ruby
+# encoding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
  # before_filter :verify_admin, :except =>[:index,:show]
+
+  helper_method :cart_content
 
   def verify_admin
     :authenticate_user!
@@ -24,5 +28,20 @@ class ApplicationController < ActionController::Base
     cart
   end
 
+  private
+  def cart_content
+    content=[]
+    priceTot=0
+    current_cart.line_items.each do |c|
+      item=Item.find_by_id(c.item_id).specify
+      quantity=c.quantity
+      price=item.price
+      priceTot += quantity*price
+      line="#{item.name} (#{quantity} x #{sprintf("%02.2f €", price)} )"
+      content << line
+     end
+   content<<"Prezzo: #{sprintf("%02.2f€",priceTot)}"
+   content
+  end
 
 end
