cea7a1432f83d798fe2d26724ebb4f9ea315d8d6
Mario <veontomo@gmail.com>
2013-05-13 10:31:37 +0200

    models/user.rb: removed association has_and_belongs_to_many :orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
-  has_and_belongs_to_many :orders
+#  has_and_belongs_to_many :orders
   before_save :setup_role
 
   # Include default devise modules. Others available are:
@@ -11,14 +11,14 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :role_ids
-  
+
   def role?(role)
       return !!self.roles.find_by_name(role.to_s.camelize)
   end
 
   # Default role is "registered"
-  def setup_role 
-    if self.role_ids.empty?     
+  def setup_role
+    if self.role_ids.empty?
       self.role_ids =  Role.find_by_name("registered").id
     end
   end
