2b607daffe2a223bda3ba65b08d6b9d7f4c403bf
Mario <veontomo@gmail.com>
2013-06-14 16:58:33 +0200

    dim all but the popup window

--- app/assets/javascripts/beers.js
+++ app/assets/javascripts/beers.js
@@ -25,13 +25,15 @@
             var oneFourth = Math.ceil($(window).width() / 4);
             $('#beerInfo').
                 html(data).
-                css({'left': oneFourth, 'width': 2*oneFourth, 'top': 50,'z-index': 1}).show();
+                css({'z-index': 1}).show();
             $('#beer_back').hide();
+            $('.panelindex').css({'opacity':0.5, 'transition': "opacity 1s ease 0.25s"});
             $('<div id="close_button">&nbsp;<div>').prependTo($('.itemcell_show')).click(spizzica.hidebeerInfo);
             return(false);  // prevent default link action
         },
         hidebeerInfo: function() {
             $('#beerInfo').hide();
+            $('.panelindex').css({'opacity':1, 'transition': "opacity 0.5s ease"});
             return(false);
         },
     }
