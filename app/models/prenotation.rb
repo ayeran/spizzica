6a68db194295c9c3241b3893b8444c769230255a
Mario <veontomo@gmail.com>
2013-09-04 12:44:21 +0200

    dropdown list is added for Prenotations:quatity

--- app/models/prenotation.rb
+++ app/models/prenotation.rb
@@ -4,6 +4,9 @@ class Prenotation
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
+ @@person_max_num = 10
+ @@person_min_num = 1
+ cattr_reader :person_max_num, :person_min_num
 
 
   attr_accessor :name, :email, :date, :quantity, :comment, :phone
@@ -13,6 +16,9 @@ class Prenotation
   validates_presence_of :name, :email, :date, :quantity, :comment, :phone
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validates_length_of :comment, :maximum => 500
+  validate :quantity,
+      :numericality => {:greater_than_or_equal => @@person_min_num,
+                        :less_then_or_equal => @@person_max_num}
 
 
   def initialize(attributes = {})
