c22c9702388ccf2fa3c18731fdcd0e61e26b8ce0
Mario <veontomo@gmail.com>
2013-09-04 20:12:56 +0200

    narrowing year selection in prenotations

--- app/controllers/prenotations_controller.rb
+++ app/controllers/prenotations_controller.rb
@@ -5,25 +5,25 @@ class PrenotationsController < ApplicationController
 
   # GET /prenotations
   # GET /prenotations.json
-  def index
-    @prenotations = Prenotation.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @prenotations }
-    end
-  end
+  # def index
+    # @prenotations = Prenotation.all
+#
+    # respond_to do |format|
+      # format.html # index.html.erb
+      # format.json { render json: @prenotations }
+    # end
+  # end
 
   # GET /prenotations/1
   # GET /prenotations/1.json
-  def show
-    @prenotation = Prenotation.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @prenotation }
-    end
-  end
+  # def show
+    # @prenotation = Prenotation.find(params[:id])
+#
+    # respond_to do |format|
+      # format.html # show.html.erb
+      # format.json { render json: @prenotation }
+    # end
+  # end
 
   # GET /prenotations/new
   # GET /prenotations/new.json
@@ -36,10 +36,10 @@ class PrenotationsController < ApplicationController
     end
   end
 
-  # GET /prenotations/1/edit
-  def edit
-    @prenotation = Prenotation.find(params[:id])
-  end
+  # # GET /prenotations/1/edit
+  # def edit
+    # @prenotation = Prenotation.find(params[:id])
+  # end
 
   # POST /prenotations
   # POST /prenotations.json
@@ -71,31 +71,31 @@ class PrenotationsController < ApplicationController
     # end
   end
 
-  # PUT /prenotations/1
-  # PUT /prenotations/1.json
-  def update
-    @prenotation = Prenotation.find(params[:id])
+  # # PUT /prenotations/1
+  # # PUT /prenotations/1.json
+  # def update
+    # @prenotation = Prenotation.find(params[:id])
+#
+    # respond_to do |format|
+      # if @prenotation.update_attributes(params[:prenotation])
+        # format.html { redirect_to @prenotation, notice: 'Prenotation was successfully updated.' }
+        # format.json { head :ok }
+      # else
+        # format.html { render action: "edit" }
+        # format.json { render json: @prenotation.errors, status: :unprocessable_entity }
+      # end
+    # end
+  # end
 
-    respond_to do |format|
-      if @prenotation.update_attributes(params[:prenotation])
-        format.html { redirect_to @prenotation, notice: 'Prenotation was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @prenotation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /prenotations/1
-  # DELETE /prenotations/1.json
-  def destroy
-    @prenotation = Prenotation.find(params[:id])
-    @prenotation.destroy
-
-    respond_to do |format|
-      format.html { redirect_to prenotations_url }
-      format.json { head :ok }
-    end
-  end
+  # # DELETE /prenotations/1
+  # # DELETE /prenotations/1.json
+  # def destroy
+    # @prenotation = Prenotation.find(params[:id])
+    # @prenotation.destroy
+#
+    # respond_to do |format|
+      # format.html { redirect_to prenotations_url }
+      # format.json { head :ok }
+    # end
+  # end
 end
--- app/models/prenotation.rb
+++ app/models/prenotation.rb
@@ -4,9 +4,15 @@ class Prenotation
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
- @@person_max_num = 10
- @@person_min_num = 1
- cattr_reader :person_max_num, :person_min_num
+  @@person_max_num = 10
+  @@person_min_num = 1
+  @@starttime = 1.hour.seconds # nearest time to make the order (in seconds): one can ask the delivery for the time (current_time + @@starttime)
+  @@duration = 1.month.seconds # latest time (in seconds) for which the order can be made: one can not ask for deliveries later than (current_time + @@starttime + @@duaration)
+
+  @@default_quantity = 4
+
+ cattr_reader :person_max_num, :person_min_num, :prenot_min_time, :starttime, :duration, :default_quantity
+
 
 
   attr_accessor :name, :email, :date, :quantity, :comment, :phone
@@ -31,4 +37,51 @@ class Prenotation
   def persisted?
     false
   end
+
+   def time_span_hash
+     {:start_year=>self.starttime.year,
+       :end_year=>self.endtime.year}
+   end
+   def starttime
+    @starttime = Time.now + @@starttime
+  end
+  def endtime
+    @endtime = Time.now + @@starttime + @@duration
+  end
+
+  def yearspan
+    ((self.starttime.year)..(self.endtime.year))
+  end
+
+  def monthspan
+    startM = self.starttime.month
+    endM = self.endtime.month
+    if endM<startM
+      endM = endM + 12
+    end
+    (@@months + @@months[1..-1])[startM..endM]
+  end
+
+  def dayspan
+    startD = self.starttime.day
+    endD = self.endtime.day
+    if endD<startD
+      endD = endD + 31
+    end
+    ((0..31).to_a + (1..31).to_a)[startD..endD].uniq
+  end
+
+  def hourspan
+    hourStart = self.starttime.hour
+    hourEnd = [23, shourStart].max
+    (hourStart..hourEnd)
+  end
+
+  def minspan
+    (0..59).step(15)
+  end
+
+
+
+
 end
